31fdc2aff43f95250c4ba1e6bd45ac04d65d3d56
ManuelDev <manueldeveloper17@gmail.com>
2023-10-07 22:02:14 -0500

    Feature: add logic to function getAllAgreements and getAgreement in agreement controller

--- back/controllers/agreement.js
+++ back/controllers/agreement.js
@@ -1,13 +1,27 @@
+const { Op } = require("sequelize");
+
 // Model
 const { Agreement } = require("../models/agreement");
 
 // Custom response
 const { success, error } = require("../network/response");
 
-// Return a list of agreements belonging to the user (buyer or supplier) where the agreements are not terminated.
+// Function to list an agreements belonging to the user (buyer or supplier) where the agreements are not terminated.
 const getAllAgreements = async (req, res) => {
   try {
-    success(req, res, "GET /agreements");
+    const notTerminatedAgreements = await Agreement.findAll({
+      where: {
+        status: {
+          [Op.not]: ["terminated"],
+        },
+      },
+    });
+
+    if (!notTerminatedAgreements || notTerminatedAgreements.length === 0) {
+      return error(req, res, "There aren't in progress or new agreements", 404);
+    }
+
+    return success(req, res, notTerminatedAgreements, 200);
   } catch (err) {
     error(req, res, err.message, 500);
   }
@@ -16,7 +30,31 @@ const getAllAgreements = async (req, res) => {
 // Create an endpoint that return the agreement only if it belongs to the calling account.
 const getAgreement = async (req, res) => {
   try {
-    success(req, res, "GET /agreements/:agreement_id");
+    const { id } = req.params;
+    const { callingAccountId } = req.body;
+
+    // TODO: Get the calling account id from the token req.user.id and verify if the account exists.
+
+    if (!id) {
+      error(req, res, "Missing id", 400);
+    }
+
+    // return the agreement only if it belongs to the calling account.
+    const agreement = await Agreement.findOne({
+      where: {
+        id,
+        [Op.or]: [
+          { BuyerId: callingAccountId },
+          { SupplierId: callingAccountId },
+        ],
+      },
+    });
+
+    if (!agreement) {
+      return error(req, res, "Agreement not found", 404);
+    }
+
+    return success(req, res, agreement, 200);
   } catch (err) {
     error(req, res, err.message, 500);
   }
--- back/network/response.js
+++ back/network/response.js
@@ -1,33 +1,24 @@
 // Function to custom success messages
-const success = (req, res, message, status, data) => {
+const success = (req, res, message, status) => {
   const statusCode = status || 200;
   const statusMessage = message || "";
-  const dataResponse = data || null;
 
   res.status(statusCode).send({
     error: false,
     status: statusCode,
     body: {
-      message: statusMessage,
-      data: dataResponse,
+      data: statusMessage,
     },
   });
 };
 
 // Function to custom error messages
-const error = (
-  req,
-  res,
-  message = "Internal Server Error",
-  status = 500,
-  data = null
-) => {
+const error = (req, res, message = "Internal Server Error", status = 500) => {
   res.status(status).send({
     error: true,
     status: status,
     body: {
-      message,
-      data,
+      data: message,
     },
   });
 };
